b629fc3f1091644ca2327bde02ed47313c87b15e
David Gasaway <ponsasinorem@gmail.com>
2013-12-14 14:29:53 -1000

    m7vzw: initial omni bringup

---
+++ omni_m7vzw.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2013 The OmniROM Project
+# Copyright (C) 2013 The CyanogenMod Project
+# Copyright (C) 2013 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit Omni CDMA telephony parts
+$(call inherit-product, vendor/omni/config/cdma.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from our omni product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# This is where we'd set a backup provider if we had one
+#$(call inherit-product, device/sample/products/backup_overlay.mk)
+
+# Discard inherited values and use our own instead.
+PRODUCT_NAME := omni_m7vzw
+PRODUCT_DEVICE := m7vzw
+PRODUCT_BRAND := htc
+PRODUCT_MANUFACTURER := HTC
+PRODUCT_MODEL := One
