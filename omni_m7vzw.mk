0d2e8344daeaeb11c5156f6f818e94eba469b8d2
Matt Mower <mowerm@gmail.com>
2013-12-17 22:02:53 -1000

    m7vzw: access vzw apps in Play store

--- omni_m7vzw.mk
+++ omni_m7vzw.mk
@@ -31,6 +31,14 @@ $(call inherit-product, device/htc/m7vzw/device.mk)
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
 
 # Discard inherited values and use our own instead.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=HTCOneVZW \
+    BUILD_ID=JDQ39 \
+    BUILD_FINGERPRINT="VERIZON/HTCOneVZW/m7wlv:4.2.2/JDQ39/221778.10:user/release-keys" \
+    PRIVATE_BUILD_DESC="1.10.605.10 CL221778 release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-verizon
+
 PRODUCT_NAME := omni_m7vzw
 PRODUCT_DEVICE := m7vzw
 PRODUCT_BRAND := htc
